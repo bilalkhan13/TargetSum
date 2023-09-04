551ab421209f726cfcfa17673325abac5897f2b4
Bilal Ahmad <bilalahmad.khan@hotmail.com>
2023-09-05 00:08:16 +0500

    :white_check_mark: ADDS: complete the basic game structure

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import Game from './src/components/Game';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-   <Game randomNumberCount={6}/>
-  );
-}
+  const [gameId, setGameId] = useState(1);
 
+  const resetGame = () => {
+    setGameId(gameId + 1);
+  }
+
+  return <Game key={gameId} onPlayAgain={resetGame} randomNumberCount={6} initialSeconds={10} />;
+}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
 
-const Game = ({ randomNumberCount }) => {
+const Game = ({ randomNumberCount, onPlayAgain }) => {
   const [selectedNumbers, setSelectedNumbers] = useState([]);
   const [randomNumbers, setRandomNumbers] = useState([]);
+  const [remainingSeconds, setRemainingSeconds] = useState(10);
+  const [shuffledNumbers, setShuffledNumbers] = useState([]);
 
   const target = randomNumbers
     .slice(0, randomNumberCount - 2)
@@ -21,18 +24,46 @@ const Game = ({ randomNumberCount }) => {
   };
 
   const gameStatus = () => {
-    const sumSelected = selectedNumbers.reduce((acc, curr)=>{
-      return acc + randomNumbers[curr]
+    const sumSelected = selectedNumbers.reduce((acc, curr) => {
+      return acc + shuffledNumbers[curr];
     }, 0);
-
-    if(sumSelected < target){
-      return 'PLAYING'
-    }else if(sumSelected === target){
-      return 'WON'
-    } else {
-      return 'LOST'
+    if (remainingSeconds === 0) {
+      return 'LOST';
     }
-  }
+    if (sumSelected < target) {
+      return 'PLAYING';
+    } else if (sumSelected === target) {
+      return 'WON';
+    } else {
+      return 'LOST';
+    }
+  };
+
+  const gameState = gameStatus();
+  useEffect(() => {
+    setShuffledNumbers(shuffle(randomNumbers));
+  }, [randomNumbers]);
+  useEffect(() => {
+    // Create the interval when the component mounts
+    const interval = setInterval(() => {
+      setRemainingSeconds((prevRemainingSeconds) => {
+        if (
+          prevRemainingSeconds === 0 ||
+          gameState === 'WON' ||
+          gameState === 'LOST'
+        ) {
+          clearInterval(interval);
+          return prevRemainingSeconds;
+        }
+        return prevRemainingSeconds - 1;
+      });
+    }, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => {
+      clearInterval(interval);
+    };
+  }, [gameState]);
 
   useEffect(() => {
     console.log('Selected Numbers Updated:', selectedNumbers);
@@ -45,23 +76,32 @@ const Game = ({ randomNumberCount }) => {
 
     setRandomNumbers(randomNumbers);
   }, [randomNumberCount]);
-  const game = gameStatus();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.target}>{target}</Text>
+      <Text style={[styles.target, styles[`STATUS_${gameState}`]]}>
+        {target}
+      </Text>
       <View style={styles.randomContainer}>
-        {randomNumbers.map((number, index) => (
+        {shuffledNumbers.map((number, index) => (
           <RandomNumber
             key={index}
             id={index}
             number={number}
-            isDisabled={isNumberSelected(index)}
+            isDisabled={isNumberSelected(index) || gameState !== 'PLAYING'}
             onPress={selectNumber}
           />
         ))}
       </View>
-      <Text>{game}</Text>
+      <View styles={styles.wrapper}>
+        <Text style={styles.timer}>Timer: {remainingSeconds}</Text>
+
+        {gameState !== 'PLAYING' && (
+          <Pressable onPress={onPlayAgain}>
+            <Text style={styles.button}>Play Again</Text>
+          </Pressable>
+        )}
+      </View>
     </View>
   );
 };
@@ -74,7 +114,6 @@ const styles = StyleSheet.create({
 
   target: {
     fontSize: 50,
-    backgroundColor: '#bbb',
     marginHorizontal: 50,
     marginVertical: 15,
     textAlign: 'center',
@@ -86,10 +125,46 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
   },
+
+  STATUS_PLAYING: {
+    backgroundColor: '#bbb',
+  },
+
+  STATUS_WON: {
+    backgroundColor: 'green',
+  },
+
+  STATUS_LOST: {
+    backgroundColor: 'red',
+  },
+
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center', // Center content horizontally
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: '#bbb',
+    textAlign: 'center',
+    width: 100,
+    fontSize: 35,
+    marginHorizontal: 'auto',
+  },
+
+  timer: {
+    width: 100,
+    fontSize: 35,
+    textAlign: 'center',
+    marginHorizontal: 'auto',
+    marginTop: 15,
+  },
 });
 
 const propTypes = {
   randomNumberCount: PropTypes.number.isRequired,
+  initialSeconds: PropTypes.number.isRequired,
+  onPlayAgain: PropTypes.func.isRequired,
 };
 
 export default Game;
